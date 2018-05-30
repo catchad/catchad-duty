878a4b777c52fc617195bf95d8f2ba261491f32c
Joseph Wang <jamesxxxxxxx@gmail.com>
2018-05-30 19:21:30 +0800

    add a lot

---
+++ src/data/employees.js
@@ -0,0 +1,40 @@
+import imageLink1 from '../images/employees/1.jpg';
+import imageLink2 from '../images/employees/2.jpg';
+import imageLink3 from '../images/employees/3.jpg';
+import imageLink4 from '../images/employees/4.jpg';
+import imageLink5 from '../images/employees/5.jpg';
+import imageLink6 from '../images/employees/6.jpg';
+import imageLink7 from '../images/employees/7.jpg';
+import imageLink8 from '../images/employees/8.jpg';
+import imageLink9 from '../images/employees/9.jpg';
+import imageLink10 from '../images/employees/10.jpg';
+import imageLink11 from '../images/employees/11.jpg';
+import imageLink12 from '../images/employees/12.jpg';
+import imageLink13 from '../images/employees/13.jpg';
+import imageLink14 from '../images/employees/14.jpg';
+import imageLink15 from '../images/employees/15.jpg';
+import imageLink16 from '../images/employees/16.jpg';
+import imageLink17 from '../images/employees/17.jpg';
+import imageLink18 from '../images/employees/18.jpg';
+
+const employees = [
+  { name: '智永 ：)', imageLink: imageLink1 },
+  { name: '彭語箴', imageLink: imageLink2 },
+  { name: '郭', imageLink: imageLink3 },
+  { name: '維晉', imageLink: imageLink4 },
+  { name: '蔡貴翰', imageLink: imageLink5 },
+  { name: '凱西尤尤（尤佳雯）', imageLink: imageLink6 },
+  { name: 'Fishhjohn', imageLink: imageLink7 },
+  { name: '張小綱', imageLink: imageLink8 },
+  { name: '張文瀚', imageLink: imageLink9 },
+  { name: 'Diane Lu', imageLink: imageLink10 },
+  { name: '陳妏伊', imageLink: imageLink11 },
+  { name: '黃植琨', imageLink: imageLink12 },
+  { name: '莊跟萄', imageLink: imageLink13 },
+  { name: 'Jerry Chen', imageLink: imageLink14 },
+  { name: '許小柔', imageLink: imageLink15 },
+  { name: 'Elsa', imageLink: imageLink16 },
+  { name: '穎', imageLink: imageLink17 },
+  { name: 'JessinLin。潔', imageLink: imageLink18 }
+];
+export default employees;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,57 @@
+import 'core-js/es6/symbol.js';
+import 'core-js/fn/symbol/iterator.js';
+// import ReactGA from 'react-ga';
+import { injectGlobal } from 'react-emotion/macro';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactGA.initialize('UA-118901514-2');
+// ReactGA.pageview('Landing');
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700');
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    line-height: 1.5;
+    outline: none;
+    font-family: 'Source Sans Pro', 'Helvetica Neue', Helvetica, 'Segoe UI', SegoeUI, Arial,
+      'PingFang TC', 'Microsoft JhengHei', sans-serif;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  li {
+    list-style-type: none;
+  }
+  button {
+    background-color: transparent;
+    border: none;
+  }
+  a {
+    text-decoration: none;
+  }
+  body {
+    width: 100%;
+    overflow-x: hidden;
+  }
+  br {
+    line-height: 0;    
+  }
+
+`;
+
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(<App />, rootEl);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(<NextApp />, rootEl);
+  });
+}
+
 registerServiceWorker();
