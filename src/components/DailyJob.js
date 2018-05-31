247d74bf7ddcea90d865011218be0e31b3faa8b8
Joseph Wang <jamesxxxxxxx@gmail.com>
2018-05-31 16:28:53 +0800

    add

---
+++ src/components/DailyJob.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import styled, { css } from 'react-emotion';
+
+class DailyJob extends Component {
+  render() {
+    return <div>123</div>;
+  }
+}
+
+export default DailyJob;
