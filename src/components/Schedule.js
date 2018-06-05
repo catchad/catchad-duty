9ac9c108ed11c93d3d33a6196ab337fa7cff5e80
Joseph Wang <jamesxxxxxxx@gmail.com>
2018-06-05 14:44:05 +0800

    drop head

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import styled from 'react-emotion';
 import Avatar from './Avatar';
 import { database } from '../firebase';
+import findIndex from 'lodash/findIndex';
+import drop from 'lodash/drop';
+import moment from 'moment';
+import 'moment/locale/zh-tw';
 
 const Container = styled.div`
   position: relative;
@@ -29,7 +33,11 @@ class Schedule extends Component {
   state = { schedule: [] };
   componentDidMount() {
     database.ref(`/schedule`).once('value', snapshot => {
-      this.setState({ schedule: snapshot.val() });
+      const currentIndex = findIndex(snapshot.val(), {
+        date: moment().format('YYYY-MM-DD')
+      });
+      const slicedData = drop(snapshot.val(), currentIndex);
+      this.setState({ schedule: slicedData });
     });
   }
 
