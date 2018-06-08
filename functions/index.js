6cb0787382dd0e4722f54b83a798468e31bd3b39
Joseph Wang <jamesxxxxxxx@gmail.com>
2018-06-08 16:46:47 +0800

    add alot

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,24 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const admin = require('firebase-admin');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+const firebaseApp = admin.initializeApp(functions.config().firebase);
+
+const database = firebaseApp.database();
+
+const getTokens = () => {
+  return database
+    .ref('/tokens')
+    .once('value')
+    .then(snapshot => snapshot.val());
+};
+
+const app = express();
+
+app.get('/tokens', (request, response) => {
+  getTokens().then(value => {
+    response.send(`${value.join(',')}`);
+  });
 });
+
+exports.app = functions.https.onRequest(app);
